e285a8ed432a3bd0baaa0d95281e8fdfc23538f9
Patrik Kempec <patrik.kempec@gmail.com>
2022-02-01 14:45:42 +0100

    Navigate To

--- src/App.js
+++ src/App.js
@@ -7,14 +7,26 @@ import Language from './components/Language/Language';
 import Location from './components/Location/Location';
 import Program from './components/Program/Program';
 import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 
 const App = () => {
 
   const [fontClass, setFontClass] = useState('svk-font');
+  const { t } = useTranslation();
 
   const center = {lat: 48.705705, lng: 21.823010};
-  const church = { lat: 48.623193, lng: 21.720706 };
-  const celebration = { lat: 48.795649, lng: 22.006125 };
+  const church = {
+    label: "1",
+    name: t('church.name'),
+    link: "https://www.google.com/maps/dir//'48.623193,21.720706'/@48.6232267,21.6854272,13z/data=!4m7!4m6!1m0!1m3!2m2!1d21.720706!2d48.623193!3e0",
+    position: { lat: 48.623193, lng: 21.720706 }
+  }
+  const celebration = {
+    label: "2",
+    name: t('celebration.name'),
+    link: "https://www.google.com/maps/dir//48.795649,+22.006125/@48.7956608,21.9710789,13z/data=!4m7!4m6!1m0!1m3!2m2!1d22.006125!2d48.795649!3e0",
+    position: { lat: 48.795649, lng: 22.006125 }
+  };
   const zoom = 10;
 
   return (
--- src/components/Location/Location.js
+++ src/components/Location/Location.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Wrapper } from "@googlemaps/react-wrapper";
 import './Location.css';
+import { useTranslation } from 'react-i18next';
 
 const Map = ({ children, style, ...options }) => {
   const ref = React.useRef(null);
@@ -32,10 +33,13 @@ const Map = ({ children, style, ...options }) => {
 
 const Marker = (options) => {
   const [marker, setMarker] = React.useState();
+  const [info, setInfo] = React.useState();
+
 
   React.useEffect(() => {
     if (!marker) {
       setMarker(new window.google.maps.Marker());
+      setInfo(new window.google.maps.InfoWindow());
     }
     return () => {
       if (marker) {
@@ -45,22 +49,33 @@ const Marker = (options) => {
   }, [marker]);
 
   React.useEffect(() => {
+
+    const generateNavigateTo = (link, text) => {
+      return `<p><a href=${link}>${text}</a></p>`;
+    }
+
     if (marker) {
       marker.setOptions(options);
+      marker.addListener("click", () => {
+        info.setContent(marker.name + generateNavigateTo(marker.link, marker.navigate));
+        info.open(marker.getMap(), marker);
+      });
     }
-  }, [marker, options]);
+  }, [marker, info, options]);
 
   return null;
 };
 
 const Location = ({ center, church, celebration, zoom }) => {
+  const { t } = useTranslation();
 
+  const navigate = t('location.navigate');
   return (
     <div className='location'>
       <Wrapper apiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
         <Map center={center} zoom={zoom} style={{ flexGrow: "1", height: "100%" }}>
-          <Marker position={church} />
-          <Marker position={celebration} />
+          <Marker position={church.position} label={church.label} name={church.name} link={church.link} navigate={navigate}/>
+          <Marker position={celebration.position} label={celebration.label} name={celebration.name} link={celebration.link} navigate={navigate}/>
         </Map>
       </Wrapper>
     </div>
