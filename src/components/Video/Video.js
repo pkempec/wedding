eb35a8d281dc56f92ae1973e55c72ca619e0f269
Patrik Kempec <patrik.kempec@gmail.com>
2022-09-13 06:36:50 +0200

    Video link

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -10,7 +10,7 @@ const Photo = () => {
     <div className='video-content'>
       <span className='video'/>
       {/* "" */}
-      <a href="https://photos.google.com/share/AF1QipOmT-07WpgBqMve5gtzfyVH6p-kfUHYKVxDQLpzLSVPbZnJk_wCT3pWrWtIBINuMg/photo/AF1QipOM8BdO3ojszihsQL1T1aGnymLTAiY1OZW1XY6_?key=T21PVmFsR2FBM24zUkNNb0RBOGxtX2VPbWJiOUNR"> {t('video.info')}</a>
+      <a href="https://youtu.be/67RkwcOD9Vg"> {t('video.info')}</a>
     </div> 
   )
 };
